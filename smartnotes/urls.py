cad3c7a38df50bab5ed3ae2d39f910a98df91a7b
Charles Wolfe <cmwolfe.dev@gmail.com>
2022-05-17 04:50:59 -0500

    Updated the urls.py file in smartnotes

--- smartnotes/urls.py
+++ smartnotes/urls.py
@@ -14,12 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
-from home import views
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # add new path
-    path('home', views.home),
+    path("", include('home.urls')),
 ]
