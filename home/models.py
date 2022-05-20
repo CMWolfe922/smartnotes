6b5c6f694018359874d43529df6507c20b60fa5c
C M Wolfe <cmwolfe.dev@gmail.com>
2022-05-20 17:15:46 -0500

    Setup the base.html file with basic html starter boilerplate code, plus added django formatting for the loading of static files and blackcontent

--- home/models.py
+++ home/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+# import the generic User object
+from django.contrib.auth.models import User
+
 # Create your models here.
+class Profile(models.Model):
+    name = models.CharField(max_length=255)
+    user = models.ForeignKey(User, related_name='profile', on_delete=models.CASCADE)
