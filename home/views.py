d82c6f11d2cdd18faf7da42d2272d6aeea721b1b
Charles M. Wolfe <cmwolfe.dev@gmail.com>
2022-05-20 07:13:39 -0500

    Created a base, index, navbar and new views in the home app. They are like the social app. Extending off base. I created an IndexView in the home apps views.py file and removed the other views

--- home/views.py
+++ home/views.py
@@ -1,16 +1,16 @@
 from django.shortcuts import render
+from django import View
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from datetime import datetime
 
 # Create your views here.
-def home(request):
-    # This is saying whenever it gets a home request return hello world
-    return render(request, "home/welcome.html", {'today': datetime.today()})
+class IndexView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'index.html', {})
 
-# Create authorized views here
-# this is all I have to do to block access to a page if user isn't logged in.
-# The login_url redirects the user to a page to login
-@login_required(login_url="/admin")
-def authorized(request):
-    return render(request, "home/authorized.html", {})
+class LoginView(View):
+    
+    @login_required(login_url='/admin')
+    def login(self, request, *args, **kwargs):
+        return render(request, 'admin.html')
